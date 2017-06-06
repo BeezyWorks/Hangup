d24b651dcae12f2defc7907a31b5101a12015c01
BeezyWorks <mattaniahbeezy@gmail.com>
2017-06-06 07:52:35 +0300

    added firebase and app icon

--- mobile/src/main/java/com/mattaniahbeezy/hangup/MainActivity.java
+++ mobile/src/main/java/com/mattaniahbeezy/hangup/MainActivity.java
@@ -8,23 +8,32 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String Tag = "MainActivity";
+    private TextView statusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusTextView= (TextView) findViewById(R.id.statusView);
         permissionCheck();
     }
 
     private void permissionCheck() {
+        String statusText = "Not Granted";
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             Log.d(Tag, "Requesting Permission");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CONTACTS}, 0);
         }
+        else{
+            statusText="Granted";
+        }
+        statusTextView.setText(statusText);
+
     }
 
     @Override
