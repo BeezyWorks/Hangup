1ab5c65ae61cbcfc3a332dc0358e29a7f18c077c
BeezyWorks <mattaniahbeezy@gmail.com>
2017-06-06 08:35:14 +0300

    added ongoing notification

--- wear/src/main/java/com/mattaniahbeezy/hangup/services/MessageReceiverService.java
+++ wear/src/main/java/com/mattaniahbeezy/hangup/services/MessageReceiverService.java
@@ -1,5 +1,9 @@
 package com.mattaniahbeezy.hangup.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
@@ -7,8 +11,7 @@ import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 import com.mattaniahbeezy.common.Constants;
 import com.mattaniahbeezy.hangup.MainActivity;
-
-import java.nio.charset.StandardCharsets;
+import com.mattaniahbeezy.hangup.R;
 
 /**
  * Created by Beezy Works Studios on 6/4/2017.
@@ -16,6 +19,7 @@ import java.nio.charset.StandardCharsets;
 
 public class MessageReceiverService extends WearableListenerService {
     private static final String TAG = "MessageReciever";
+    private static final int NOTIFICAITON_ID = 0;
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
@@ -23,16 +27,13 @@ public class MessageReceiverService extends WearableListenerService {
             case Constants.START_WATCH_APP_PATH:
                 startMainActivity();
                 break;
-            case Constants.PHONE_NUMBER_PATH:
-                updatePhoneNumber(messageEvent);
-                break;
             case Constants.END_WATCH_APP_PATH:
                 endMainActivity();
                 break;
         }
     }
 
-    private Intent getMainActivityIntent(){
+    private Intent getMainActivityIntent() {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         return intent;
@@ -41,20 +42,29 @@ public class MessageReceiverService extends WearableListenerService {
     private void startMainActivity() {
         Log.d(TAG, "received start activity command");
         startActivity(getMainActivityIntent());
+        createNotification();
     }
 
-    private void endMainActivity(){
+    private void endMainActivity() {
         Log.d(TAG, "received kill activity command");
         Intent intent = getMainActivityIntent();
         intent.putExtra(MainActivity.KEY_KILL_SELF, true);
         startActivity(intent);
+        killNotification();
     }
 
-    private void updatePhoneNumber(MessageEvent messageEvent) {
-        String phoneNumber = new String(messageEvent.getData(), StandardCharsets.UTF_8);
-        Log.d(TAG, "received phone number " + phoneNumber);
-        Intent intent = getMainActivityIntent();
-        intent.putExtra(MainActivity.KEY_NEW_PHONE, phoneNumber);
-        startActivity(intent);
+    private void createNotification() {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, getMainActivityIntent(), PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Ongoing Call")
+                .setContentIntent(pendingIntent)
+                .setOngoing(true)
+                .build();
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).notify(NOTIFICAITON_ID, notification);
+    }
+
+    private void killNotification(){
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).cancel(NOTIFICAITON_ID);
     }
 }
